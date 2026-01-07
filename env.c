994ba4e252b7c578faa7a80f27f88033f840650b
Pawnee <pawneedefize@hotmail.com>
2026-01-07 14:53:06 +0000

    resolve conficlt

--- env.c
+++ env.c
@@ -3,7 +3,7 @@
 * env - Prints all environment variables
 * @env: Array of environment variables
 */
-void print_env(char **env)
+void print_env(char **env, char **argv, char *line)
 {
 	int read = 0;
 
@@ -12,4 +12,10 @@ void print_env(char **env)
 		printf("%s\n", env[read]);
 		read++;
 	}
+	if (argv && argc[0] && strcmp(argv[0], "exit") == 0)
+    {
+        free(args);
+        free(line);
+        exit(last_status);
+    }
 }
